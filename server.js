b10d50953380419cad02d2ada3d961ae998485ed
lyamz <davidlyangenda@gmail.com>
2022-03-24 03:46:25 +0200

    fix linters

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const webpack = require("webpack");
-const webpackDevMiddleware = require("webpack-dev-middleware");
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
-const config = require("./webpack.config.js");
+const config = require('./webpack.config.js');
 
 const compiler = webpack(config);
 
@@ -17,5 +17,5 @@ app.use(
 
 // Serve the files on port 3000.
 app.listen(3000, () => {
-  console.log("Example app listening on port 3000!\n");
+  console.log('Example app listening on port 3000!\n');
 });
--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import "./style.css";
 class Tasks {
   constructor() {
     this.tasks = [
-      { description: "Pray", completed: false, index: 0 },
-      { description: "Read", completed: false, index: 1 },
+      { description: 'Pray', completed: false, index: 0 },
+      { description: 'Read', completed: false, index: 1 },
     ];
   }
 
   generate = () => {
     const dell = this.tasks
       .map((task) => {
-        const htmlText = ` <li class="task">
+        const htmlText = ` <li class='task'>
                <div> <button type="button"><i class="fa-regular fa-square" ${
                  task.completed
                } ></i></button>
@@ -21,7 +21,7 @@ class Tasks {
         return htmlText;
       })
       .join(" ");
-    const container = document.getElementById("tasks");
+    const container = document.getElementById('tasks');
     container.innerHTML = dell;
   };
 
@@ -35,11 +35,11 @@ const taskList = new Tasks();
 taskList.generate();
 
 const addTask = () => {
-  const input = document.getElementById("add");
-  input.addEventListener("change", (e) => {
+  const input = document.getElementById('add');
+  input.addEventListener('change', (e) => {
     e.preventDefault();
     taskList.newTask(input.value);
-    input.value = "";
+    input.value = '';
   });
 };
 
