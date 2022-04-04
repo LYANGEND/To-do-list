cb9d6d349ef273ca13cc6c4ac22601eed596c1ed
lyamz <davidlyangenda@gmail.com>
2022-04-04 14:12:45 +0200

    deploy web to github pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: '[name].bundle.js', 
-    path: path.resolve(__dirname, 'dist'), 
+    path: path.resolve(__dirname, 'docs'), 
     clean: true, 
     publicPath: '/',
   },
