1e45841a2f209ae2063b6f9653f57d1155e416d8
Basit Arigbabuwo <arigbabuwobasitkorede6@gmail.com>
2022-04-07 19:50:31 +0100

    Add test that check if data has stored in localStorage

--- Test/addRemove.test.js
+++ Test/addRemove.test.js
@@ -2,30 +2,30 @@
  * @jest-environment jsdom
  */
 
-import addTask from "../modules/addTask.js";
-import removeTask from "../modules/removeTask.js";
+import addTask from '../modules/addTask.js';
+import removeTask from '../modules/removeTask.js';
 
-describe("addTask", () => {
-  test("Add a new item to the list", () => {
+describe('addTask', () => {
+  test('Add a new item to the list', () => {
     document.body.innerHTML = `<div class="task-list">
          <input type="checkbox" class="checkbox" ><input type="text" class="task"><i class="fa fa-ellipsis-v visible" ></i><i class="fa fa-trash invisible"></i>
  
        </div>
        <hr>`;
-    addTask("add a removeItem function");
-    const list = document.querySelectorAll(".task-list");
+    addTask('add a removeItem function');
+    const list = document.querySelectorAll('.task-list');
     expect(list).toHaveLength(1);
   });
 
-  test("verify local storage after invoking addItem function ", () => {
-    const storage = JSON.parse(localStorage.getItem("tasks"));
+  test('verify local storage after invoking addItem function ', () => {
+    const storage = JSON.parse(localStorage.getItem('tasks'));
     expect(storage.length).toBe(1);
   });
 
-  test("remove an item from the list", () => {
-    const storage = JSON.parse(localStorage.getItem("tasks"));
+  test('remove an item from the list', () => {
+    const storage = JSON.parse(localStorage.getItem('tasks'));
     document.body.innerHTML = '<div id="show-tasks"></div>';
-    const container = document.getElementById("show-tasks");
+    const container = document.getElementById('show-tasks');
     storage.forEach(() => {
       container.innerHTML += `<div class="task-list">
                                      <input type="checkbox" class="checkbox" ><input type="text" class="task"><i class="fa fa-ellipsis-v visible" ></i><i class="fa fa-trash invisible"></i>
@@ -34,12 +34,12 @@ describe("addTask", () => {
                                    <hr>`;
     });
     removeTask(1);
-    const list = document.querySelectorAll(".task-list");
+    const list = document.querySelectorAll('.task-list');
     expect(list).toHaveLength(1);
   });
 
-  /*test('verify local storage after invoking removeItem function ', () => {
-         const storage = JSON.parse(localStorage.getItem('tasks'));
-         expect(storage.length).toBe(1);
-     });*/
+  test('verify local storage after invoking removeItem function ', () => {
+    const storage = JSON.parse(localStorage.getItem('tasks'));
+    expect(storage.length).toBe(1);
+  });
 });
